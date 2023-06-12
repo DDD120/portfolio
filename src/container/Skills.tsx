4446b4eecdf0e1b0cad8899dc115536d7cd87816
DDD120 <dltnals9794@naver.com>
2023-06-12 15:23:34 +0900

    feat Skills 업데이트

--- src/container/Skills.tsx
+++ src/container/Skills.tsx
@@ -1,14 +1,16 @@
-import Heading from "@components/Heading";
-import SkillItem from "@components/SkillItem";
+import Heading from "@components/Heading"
+import SkillItem from "@components/SkillItem"
 
 const skills = [
-  "HTML",
-  "CSS",
+  "HTML5",
+  "CSS3",
   "JavaScript",
   "TypeScript",
-  "React",
+  "React.js",
+  "Next.js",
   "Styled-Components",
   "Emotion",
+  "Tailwind CSS",
   "Node.js",
   "Express",
   "Jest",
@@ -16,7 +18,7 @@ const skills = [
   "Git",
   "Photoshop",
   "Figma",
-];
+]
 
 function Skills() {
   return (
@@ -24,14 +26,13 @@ function Skills() {
       <Heading>SKILLS</Heading>
       <div className="w-full font-scd text-lg my-12 text-center break-keep max-sm:text-base">
         <p>시맨틱 마크업을 고려한 HTML 마크업</p>
-        <p>CSS 미디어쿼리를 사용한 반응형 레이아웃</p>
+        <p>CSS 미디어 쿼리를 사용한 반응형 레이아웃</p>
         <p>ES6+ 문법을 사용한 JavaScript 작성</p>
         <p>TypeScript를 사용한 타입 시스템 적용</p>
-        <p>React를 사용한 컴포넌트 단위의 개발</p>
-        <p>Styled-Components, emotion, tailwind css을 사용한 UI Styling</p>
+        <p>React.js를 사용한 컴포넌트 단위의 개발</p>
+        <p>Emotion, Tailwind CSS 등 CSS 프레임워크를 사용한 UI Styling</p>
         <p>사용자 경험을 고려하는 UI Component 구현</p>
-        <p>React Context API 와 Redux를 사용한 상태관리</p>
-        <p>HTTP REST API 요청 및 응답 처리</p>
+        <p>HTTP REST API를 사용한 데이터 요청 및 응답 처리</p>
         <p>웹 성능 최적화 및 개선</p>
       </div>
       <div className="flex justify-center gap-2 flex-wrap w-96 mb-8 max-sm:text-base">
@@ -40,7 +41,7 @@ function Skills() {
         ))}
       </div>
     </section>
-  );
+  )
 }
 
-export default Skills;
+export default Skills
